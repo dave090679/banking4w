d242f02086d1f31be878994bb2cfc0f90c95b45d
David Parduhn <xkill85@gmx.net>
2017-11-08 12:54:30 +0100

    initial version

---
+++ addon/appModules/topbanking.py
@@ -0,0 +1,24 @@
+#appModules/topbanking.py
+#A part of NonVisual Desktop Access (NVDA)#Copyright (C) 2006-2012 NVDA Contributors
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+import appModuleHandler
+from config import conf
+from NVDAObjects.IAccessible import IAccessible
+from NVDAObjects.UIA import ListItem
+import controlTypes
+class topbankingcell(IAccessible):
+	def _get_name(self):
+		linenumbers = conf["documentFormatting"]["reportLineNumber"]
+		origname = self.IAccessibleObject.accName()
+		linenumber = self.parent.IAccessibleObject.accName()
+		if linenumbers == True:
+			ret = origname
+		else:
+			ret = origname.replace(linenumber,u'')
+		return ret
+
+class AppModule(appModuleHandler.AppModule):
+	def chooseNVDAObjectOverlayClasses(self, obj, clslist):
+		if obj.role == controlTypes.ROLE_TABLECELL:
+				clslist.insert(0, topbankingcell)
---
+++ addon/doc/de/readme.md
@@ -0,0 +1,7 @@
+# NVDA-Erweiterung für Subsembly Banking #
+
+Diese Erweiterung verbesert die Zugänglichkeit von Subselbly Banking Version 6.x. 
+
+## Verwendung
+
+Verwenden Sie die Option "Zeilennummern anzeigen" in den Einstellungen für Dokumentformatierungen, um die Anzeige von Zeilennummern in diversen Tabellarischen Ansichten (wie z.B. in den Umsätzen) ein- oder auszuschalten.
