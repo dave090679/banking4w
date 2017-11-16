334032f5fa539af38fde9dc4145ea966d34c9d69
David Parduhn <xkill85@gmx.net>
2017-11-16 22:24:50 +0100

    Version 0.3: Namen von registerkarten in den Einstellungen werden richtig angezeigt.

--- buildVars.py
+++ buildVars.py
@@ -20,7 +20,7 @@ addon_info = {
 	"addon_description" : _("""nvda add-on for banking4w from subsemblya.com
 	"""),
 	# version
-	"addon_version" : "0.2",
+	"addon_version" : "0.3",
 	# Author(s)
 	"addon_author" : u"David Parduhn <xkill85@gmx.net>",
 	# URL for the add-on documentation support
